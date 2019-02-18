8ef34a55c25a0f8dd54793c157f66cc2ca3cbcdd
Saeed <saeed.hadikhanloo@gmail.com>
2019-02-18 17:37:53 +0100

    A permutation function is written and applied to the data basis

--- rearrange.hs
+++ rearrange.hs
@@ -1,23 +1,31 @@
+import System.IO
 
-ml = length y
+--melem :: [a] -> Int -> Maybe a
+melem :: [a] -> Int -> a
+--melem [] _ = Nothing
+melem (x:xs) n
+--   | n <= 0 = Nothing
+   | n ==1 = x
+   | otherwise = melem xs (n-1)
+
+mreplace :: [a] -> Int -> a -> [a]
+mreplace [] _ _ = []
+mreplace (x:xs) n a
+ | n <= 0 = []
+ | n == 1 = [a] ++ xs
+ | otherwise = [x] ++ mreplace xs (n-1) a
 
 
-mzip :: [a] -> [b] -> [(a,b)]
-mzip [] _ = []
-mzip _ [] = []
-mzip (x:xs) (y:ys) = [(x,y)] ++ mzip xs ys
 
-mpermuteinit :: [(Int, a)] -> [(Int, a)]
-mpermuteinit y = [ (k,elemm) | (n,elemm) <- y let k = (2*n+1)%ml]
+mpermutefun ::  Integral a => a -> a -> a
+mpermutefun n m = 1 + mod (2*m+1) n
 
-mput :: [(Int, a)] -> [a]
-mput y = foldl [1..ml]
 
+--mpermute :: [a] -> [Maybe a]
 mpermute :: [a] -> [a]
-mpermute x = (mzip [1..] x)
-y = mzip [1..] x
-ynew =
+mpermute x = let ml=length x in [ melem x (mpermutefun ml m) | m <- [1..ml] ]
 
 main = do
-  contents <- getContents
-  mlines = lines contents
+  contents <- readFile "devCards.csv"
+  let mlines = unlines . mpermute . lines $ contents
+  writeFile "devCardsRearr.csv" mlines
